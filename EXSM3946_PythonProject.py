c02887e1d669e803b6f03a2ef930bacf64767ffc
arvpandit <pandit.arv@gmail.com>
2022-09-25 15:16:01 -0600

    Update EXSM3946_PythonProject.py

--- EXSM3946_PythonProject.py
+++ EXSM3946_PythonProject.py
@@ -1,1 +1,16 @@
-print("Hello World")
+print("Welcome to the chartmaker! Please begin by choosing one of the following options: \nOption 1: Manual Data Entry \nOption 2: Enter Data From Text File \nOption 0: to Quit the program.")
+
+
+while True:
+    userOption = input("Please Enter your Option: ")
+    match userOption:
+        case "1":
+            print("Manual Data Entry.")
+        case "2":
+            print("Entry Data from text File.")
+        case "0":
+            # bool(int("0"))
+            print("Good Bye")
+            break
+        case _:
+            print("Sorry this option is not available")
