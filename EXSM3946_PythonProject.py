66d81fde3b986cefc2f5f3183c6fdaecddccfa32
Arv <pandit.arv@gmail.com>
2022-09-26 06:58:29 -0600

    x and y axis plot list ; plot title , x and y axis label;

--- EXSM3946_PythonProject.py
+++ EXSM3946_PythonProject.py
@@ -1,6 +1,9 @@
 # from warnings import catch_warnings
 
 
+from ast import match_case
+
+
 print("Welcome to the chartmaker! Please begin by choosing one of the following options: \nOption 1: Manual Data Entry \nOption 2: Enter Data From Text File")
 def isFloat(prompt, vari):
         isTrue = False
@@ -14,6 +17,17 @@ def isFloat(prompt, vari):
                         isTrue = True
                         isFloat(prompt, vari)
 
+def quesAns(title):
+        while True:
+                userSelection = input(f'Would you like a {title}? ').strip().upper()
+                if userSelection == "Y":
+                        variableName = input(f'Please enter {title} ').strip().capitalize()
+                        return variableName
+                elif userSelection == "N":
+                        break 
+                else:
+                        print("Please Enter 'Y' or 'N'.")
+
 x_axis = list()
 y_axis = list()
 
@@ -26,16 +40,34 @@ while True:
                         # x_AxisEntry = ""
                         while userOption.upper() != "E":
                                 userOption = input("Press 'Enter' to add value in x-axis List or 'E' to exit: ")
-                                if userOption is not "E":
+                                if userOption != "E":
                                         isFloat("Please Enter x-axis values: ", x_axis)
 
                         while userOption.upper() != "Q":
                                 userOption = input("Press 'Enter' to add value in y-axis List or 'Q' to exit: ")
                                 if userOption is not "Q":
                                         isFloat("Please Enter y-axis values: ", y_axis)
-                                        
+
                         print( x_axis)
                         print( y_axis)
+                        plotTitle = quesAns("plot title")
+                        x_axisLabel = quesAns("x-axis label")
+                        y_axisLabel = quesAns("y-axis label")                
+                        
+                        print(plotTitle)
+                        print(x_axisLabel)
+                        print(y_axisLabel)
+
+                        while True:
+                                userOption = input("Would you like to choose a custom line style? Y/N  ").strip().upper()
+                                match userOption:
+                                        case 'Y':
+                                                print("Choose from the options")
+                                        case 'N':
+                                                break
+                                        case _:
+                                                print('Please Choose Y/N.')
+
 
                 case "2":
                         print("Entry Data from text File.")
