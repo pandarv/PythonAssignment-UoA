a58c072fe6b2c873db3f53e50dd949cc56444d71
Arv <pandit.arv@gmail.com>
2022-09-26 22:10:42 -0600

    created a function to remove DRY for marker style and line style

--- EXSM3946_PythonProject.py
+++ EXSM3946_PythonProject.py
@@ -27,6 +27,28 @@ def quesAns(title):
                 else:
                         print("Please Enter 'Y' or 'N'.")
 
+def gettingStyles(name, opOne, one,  opTwo, two, opThree, three, opFour, four):
+        userStyle = None
+        while True:
+                                userOption = input(f'Would you like to choose a custom {name}? Y/N  ').strip().upper()
+                                if userOption == 'Y':
+                                        print(f'Option 1: {opOne} \nOption 2: {opTwo} \nOption 3: {opThree} \nOption 4: {opFour}')
+                                        userOption = input().strip().upper()
+                                        if userOption == "1":
+                                                userStyle = one
+                                        elif userOption == "2":
+                                                userStyle = two
+                                        elif userOption == "3":
+                                                userStyle = three
+                                        elif userOption == "4":
+                                                userStyle = four
+                                        break
+                                elif userOption == "N":
+                                        break
+                                else:
+                                        print('Please Choose Y/N.')
+        return userStyle        
+
 x_axis = list()
 y_axis = list()
 
@@ -49,50 +71,51 @@ while True:
                         plotTitle = quesAns("plot title")
                         x_axisLabel = quesAns("x-axis label")
                         y_axisLabel = quesAns("y-axis label")
-                        lineStyle = None
-                        markerStyle = None                
+                        lineStyle = gettingStyles(name = "line Style", opOne = "Solid Line", one = '-',  opTwo = "Dotted Line", two = ':', opThree = "Dashed Line", three = '--',  opFour = "Dashed/Dotted Line", four = '-.')
+                        markerStyle = gettingStyles(name = "marker Style", opOne = "Circle Marker", one = 'o',  opTwo = "Star Marker", two = '*', opThree = "Diamond Marker", three = 'D',  opFour = "Hexagon Marker", four = 'H')                
                         
                         print(plotTitle)
                         print(x_axisLabel)
                         print(y_axisLabel)
                         
-                        while True:
-                                userOption = input("Would you like to choose a custom line style? Y/N  ").strip().upper()
-                                if userOption == 'Y':
-                                        print("Option 1: Solid Line \nOption 2: Dotted Line \nOption 3: Dashed Line \nOption 4: Dashed/Dotted Line")
-                                        userOption = input().strip().upper()
-                                        if userOption == "1":
-                                                lineStyle = '-'
-                                        elif userOption == "2":
-                                                lineStyle = ':'
-                                        elif userOption == "3":
-                                                lineStyle = '--'
-                                        elif userOption == "4":
-                                                lineStyle = '-.'
-                                        break
-                                elif userOption == "N":
-                                        break
-                                else:
-                                        print('Please Choose Y/N.')
+                        # while True:
+                        #         userOption = input("Would you like to choose a custom line style? Y/N  ").strip().upper()
+                        #         if userOption == 'Y':
+                        #                 print("Option 1: Solid Line \nOption 2: Dotted Line \nOption 3: Dashed Line \nOption 4: Dashed/Dotted Line")
+                        #                 userOption = input().strip().upper()
+                        #                 if userOption == "1":
+                        #                         lineStyle = '-'
+                        #                 elif userOption == "2":
+                        #                         lineStyle = ':'
+                        #                 elif userOption == "3":
+                        #                         lineStyle = '--'
+                        #                 elif userOption == "4":
+                        #                         lineStyle = '-.'
+                        #                 break
+                        #         elif userOption == "N":
+                        #                 break
+                        #         else:
+                        #                 print('Please Choose Y/N.')
                         
-                        while True:
-                                userOption = input("Would you like to choose a custom marker style? Y/N  ").strip().upper()
-                                if userOption == 'Y':
-                                        print("Option 1: Circle Marker \nOption 2: Star Marker \nOption 3: Diamond Marker \nOption 4: Hexagon Marker")
-                                        userOption = input().strip().upper()
-                                        if userOption == "1":
-                                                markerStyle = 'o'
-                                        elif userOption == "2":
-                                                markerStyle = '*'
-                                        elif userOption == "3":
-                                                markerStyle = 'D'
-                                        elif userOption == "4":
-                                                markerStyle = 'H'
-                                        break
-                                elif userOption == "N":
-                                        break
-                                else:
-                                        print('Please Choose Y/N.')
+                        # while True:
+                        #         userOption = input("Would you like to choose a custom marker style? Y/N  ").strip().upper()
+                        #         if userOption == 'Y':
+                        #                 print("Option 1: Circle Marker \nOption 2: Star Marker \nOption 3: Diamond Marker \nOption 4: Hexagon Marker")
+                        #                 userOption = input().strip().upper()
+                        #                 if userOption == "1":
+                        #                         markerStyle = 'o'
+                        #                 elif userOption == "2":
+                        #                         markerStyle = '*'
+                        #                 elif userOption == "3":
+                        #                         markerStyle = 'D'
+                        #                 elif userOption == "4":
+                        #                         markerStyle = 'H'
+                        #                 break
+                        #         elif userOption == "N":
+                        #                 break
+                        #         else:
+                        #                 print('Please Choose Y/N.')
+                        
                         print("Line Style: " , lineStyle)
                         print("Marker: " , markerStyle)
                         plt.plot(x_axis, y_axis, linestyle = lineStyle, marker = markerStyle)
@@ -104,10 +127,10 @@ while True:
                         y_axis.clear()
 
                 case "2":
-                        print("Entry Data from text File.")
+
                         floatData= list()
-                        x_axis = floatData[0]
-                        y_axis = floatData[1]
+                        x_axis = None
+                        y_axis = None
                         try:
                                 fileName = input("Please enter the file name: ")
                                 fileData = open(f'{fileName}', 'r')
@@ -116,10 +139,10 @@ while True:
                                         floatList = [float(x) for x in lengthOfData]
                                         floatData.append(floatList)
                                         print(floatList)
-
                         except Exception:
-                                # print("Could not read the file: ", fname)
-                                print("Error: ", Exception)
+                                print("Error: File Does not exist. ", Exception)
+                        if len(floatData):
+                                [x_axis, y_axis] = floatData
 
                         print(floatData)
                         print(x_axis)
